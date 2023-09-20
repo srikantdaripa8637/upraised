74348a3cc084b3591a53b58f1e7945042f8234d8
srikant <srikantdaripa8637@gmail.com>
2023-09-20 18:04:55 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          marginBottom: 10,
+        }}
+      >
+
+        <a
+          href="https://www.upraised.co/"
+          style={{ cursor: "pointer" }}
+        >
+          Upraised 
+        </a>
+      </div>
+    );
+  };
+  
+  export default Footer;
